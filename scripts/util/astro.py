f0745245b364a11b52657467348f8f8c4fc68a0e
Chris Hronek <pohek321@gmail.com>
2022-06-21 14:17:16 -0600

    Add supporting methods to astro client

--- scripts/util/astro.py
+++ scripts/util/astro.py
@@ -26,7 +26,18 @@ class astroClient():
         })
         response = requests.post(url=url, headers=self._build_headers(), data=payload)
         if response.status_code == 200:
-            print(f"Pool {name} successfully migrated")
+            print(f"Pool {name} successfully created")
+
+    def update_pool(self, name, slots, description=None):
+        url = f"{self.domain}/api/v1/pools/{name}"
+        payload = json.dumps({
+            "name": name,
+            "slots": slots,
+            "description": description
+        })
+        response = requests.patch(url=url, headers=self._build_headers(), data=payload)
+        if response.status_code == 200:
+            print(f"Pool {name} successfully updated")
 
     def create_connection(self, conn_id, conn_type, host, login, schema, port, extra,description):
         url = f"{self.domain}/api/v1/connections"
@@ -43,7 +54,23 @@ class astroClient():
         })
         response = requests.post(url=url, headers=self._build_headers(), data=payload)
         if response.status_code == 200:
-            print(f"Connection {conn_id} successfully migrated - please fill in password field manually")
+            print(f"Connection {conn_id} successfully created - please fill in password field manually")
+
+    def update_connection(self, conn_id, conn_type, host, login, schema, port, extra, description):
+        url = f"{self.domain}/api/v1/connections/{conn_id}"
+        payload = json.dumps({
+            "connection_id": conn_id,
+            "conn_type": conn_type,
+            "host": host,
+            "login": login,
+            "schema": schema,
+            "port": port,
+            "extra": extra,
+            "description": description
+        })
+        response = requests.patch(url=url, headers=self._build_headers(), data=payload)
+        if response.status_code == 200:
+            print(f"Connection {conn_id} successfully updated - please fill in password field manually")
 
     def create_variable(self, key, value):
         url = f'{self.domain}/api/v1/variables'
@@ -53,12 +80,37 @@ class astroClient():
         })
         response = requests.post(url=url, headers=self._build_headers(), data=payload)
         if response.status_code == 200:
-            print(f"Variable {response.json()['key']} succcessfully migrated")
+            print(f"Variable {response.json()['key']} succcessfully created")
+
+    def update_variable(self, key, value):
+        url = f'{self.domain}/api/v1/variables/{key}'
+        payload = json.dumps({
+            "key": key,
+            "value": value
+        })
+        response = requests.patch(url=url, headers=self._build_headers(), data=payload)
+        if response.status_code == 200:
+            print(f"Variable {response.json()['key']} succcessfully updated")
             
     def list_providers(self):
         url = f'{self.domain}/api/v1/providers'
         response = requests.get(url=url, headers=self._build_headers()).json()
         return response['providers']
+
+    def list_connections(self):
+        url = f'{self.domain}/api/v1/connections'
+        response = requests.get(url=url, headers=self._build_headers()).json()
+        return response['connections']
+
+    def list_pools(self):
+        url = f'{self.domain}/api/v1/pools'
+        response = requests.get(url=url, headers=self._build_headers()).json()
+        return response['pools']
+
+    def list_variables(self):
+        url = f'{self.domain}/api/v1/variables'
+        response = requests.get(url=url, headers=self._build_headers()).json()
+        return response['variables']
 
     def _build_headers(self):
         authorized_headers = self.headers
