5d723c44dd8d348656dcc32d8206c00173cb814b
Frank Breetz <frank.breetz@gmail.com>
2022-10-13 11:03:50 -0400

    Update migrate_conn.py

--- dags/v1_10to2x/migrate_conn.py
+++ dags/v1_10to2x/migrate_conn.py
@@ -14,23 +14,33 @@ from airflow.models import Variable
 
 def migrate_conns():
     with db.create_session() as session:
-        variables: List[Variable] = session.query(Variable).all()
-        # connections = session.query(Connection).all()
-    for variable in variables:
-        variable_data = {
-            "key": variable.key,
-            "value": variable.val,
-        }
+        connections = session.query(Connection).all()
 
-
+    conn_list = [
+        [getattr(c, column.name) for column in Connection.__mapper__.columns]
+        for c in connections
+    ]
+    for conn in conn_list:
+        formatted_conn = {
+                    "connection_id": conn[3],
+                    "conn_type": conn[4],
+                    "host": conn[5],
+                    "login": conn[7],
+                    "schema": conn[6],
+                    "port": int(conn[8]) if conn[8] else None,
+                    "password": conn[0] if conn[0] else "",
+                    "extra": str(conn[1])
+                }
         url = Variable.get("ASTRO_URL")
         token = Variable.get("ASTRO_ACCESS_KEY")
         headers = {'Content-type': 'application/json', 'Authorization': f'Bearer {token}', 'Accept': 'application/json'}
-        r = requests.post(f'{url}/api/v1/variables', data=json.dumps(variable_data), headers=headers)
+        r = requests.post(f'{url}/api/v1/connections', data=json.dumps(formatted_conn), headers=headers)
+        print(conn[3])
         print(r.text)
+    print(conn_list)
 
 with DAG(
-    dag_id="migrate_vars",
+    dag_id="migrate_conns",
     schedule_interval=None,
     start_date=datetime(2022, 1, 1),
     catchup=False,
